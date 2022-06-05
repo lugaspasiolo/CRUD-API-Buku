5ad3fa75ea2ed0276b52d9c9cfff361054b354b7
lugaspasiolo <76673759+lugaspasiolo@users.noreply.github.com>
2022-06-05 21:22:06 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,16 @@
+<?php
+$dbserver = 'localhost';
+$dbname = 'kuliahweb';
+$dbuser = 'root';
+$dbpassword = '';
+$dsn = "mysql:host={$dbserver};dbname={$dbname}";
+
+$connection = null;
+
+try{
+    $connection = new PDO ($dsn, $dbuser, $dbpassword);
+
+}catch(Exception $exception){
+    die("terjadi error: {$exception->getMessage()}");
+}
+?>
